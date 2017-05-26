203fdc1d5e9e9c7e299a4209cad8cadac4e51fa4
aamfahrurr <aam.fahrur@ti.stmikdb.ac.id>
2017-05-26 15:07:12 +0700

    Create test.sh

---
+++ test.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+# initialisasi var
+OS=`uname -p`;
+
+# go to root
+cd
+
+# install openvpn
+wget -O /etc/openvpn/openvpn.tar "https://raw.github.com/choirulanam217/script/master/conf/openvpn-debian.tar"
+cd /etc/openvpn/
+tar xf openvpn.tar
+wget -O /etc/openvpn/1194.conf "https://raw.github.com/choirulanam217/script/master/conf/1194-centos.conf"
+if [ "$OS" == "x86_64" ]; then
+  wget -O /etc/openvpn/1194.conf "https://raw.github.com/choirulanam217/script/master/conf/1194-centos64.conf"
+fi
+wget -O /etc/iptables.up.rules "https://raw.github.com/choirulanam217/script/master/conf/iptables.up.rules"
+sed -i '$ i\iptables-restore < /etc/iptables.up.rules' /etc/rc.local
+sed -i '$ i\iptables-restore < /etc/iptables.up.rules' /etc/rc.d/rc.local
+MYIP=`curl -s ifconfig.me`;
+MYIP2="s/xxxxxxxxx/$MYIP/g";
+sed -i $MYIP2 /etc/iptables.up.rules;
+sed -i 's/venet0/eth0/g' /etc/iptables.up.rules
+iptables-restore < /etc/iptables.up.rules
+sysctl -w net.ipv4.ip_forward=1
+sed -i 's/net.ipv4.ip_forward = 0/net.ipv4.ip_forward = 1/g' /etc/sysctl.conf
+service openvpn restart
+chkconfig openvpn on
+cd
+
+# configure openvpn client config
+cd /etc/openvpn/
+wget -O /etc/openvpn/1194-client.ovpn "https://raw.github.com/choirulanam217/script/master/conf/1194-client.conf"
+sed -i $MYIP2 /etc/openvpn/1194-client.ovpn;
+PASS=`cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 15 | head -n 1`;
+useradd -M -s /bin/false Choirul
+echo "Choirul:$PASS" | chpasswd
+echo "Choirul" > pass.txt
+echo "$PASS" >> pass.txt
+tar cf client.tar 1194-client.ovpn pass.txt
+cp client.tar /home/vps/public_html/
+cd
